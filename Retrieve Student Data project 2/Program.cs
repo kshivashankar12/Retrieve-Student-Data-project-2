c1ab5ae6f149797a1f73f02c76ac2e52b5470947
Kshivashankar12 <kshivashankargowda1999@gmail.com>
2023-10-24 18:04:23 +0530

    Add project files.

---
+++ Retrieve Student Data project 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = @"C:\Users\kshiv\OneDrive\Documents\StudentData.txt";
+
+
+        // Check if the file exists
+        if (File.Exists(filePath))
+        {
+            // Read the contents of the file
+            string fileContents = File.ReadAllText(filePath);
+
+            // Display the contents of the file on the console
+            Console.WriteLine(fileContents);
+        }
+        else
+        {
+            Console.WriteLine("The file does not exist.");
+        }
+
+        // Wait for the user to press any key before exiting
+        Console.ReadKey();
+    }
+}
